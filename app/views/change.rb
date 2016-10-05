421308889516373b489373130ca838ad1f2a3485
zhuwenqian <1506675573@qq.com>
2016-10-05 19:47:36 +0800

    asdf

--- app/views/change.rb
+++ app/views/change.rb
@@ -1,15 +1,1 @@
-def traverse_dir(file_path)
-  if File.directory? file_path
-    Dir.foreach(file_path) do |file|
-      if file !="." and file !=".."
-        traverse_dir(file_path+"/"+file)
-      end
-    end
-  else
-    #puts "File:#{File.basename(file_path)}, Size:#{File.size(file_path)}"
-    puts file_path
-    system("slimrb --rails -e #{file_path} > #{file_path}.sub('slim','html')")
-  end
-end
-traverse_dir('/home/bob/workspace/github/alice_blog/app/views')
 
